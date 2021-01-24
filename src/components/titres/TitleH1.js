f572580d0ab42831a9086fdf6998181e53b2a933
Gasana.NHonore <ndayiho5@gmail.com>
2021-01-24 13:08:57 +0100

    first commit , installation bootstrab + initilaisation composant

---
+++ src/components/titres/TitleH1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const titleH1 = (props) => (
+        <h1 className="border border-dark bg-primary p-2 m-2 rounded text-center text-white">{props.children}</h1>
+    );
+
+
+export default titleH1
