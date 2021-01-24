26ba960e60c1f0ff73221d52f07f70998ccdf61e
Gasana.NHonore <ndayiho5@gmail.com>
2021-01-24 13:41:55 +0100

    creation des compôsant avec snippet

---
+++ src/components/Bouton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const button = (props) => {
+     return(
+         <>
+
+         </>
+     );
+}
+
+export default button
---
+++ src/containers/Livres.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Livres extends Component {
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+export default Livres
