16e1b073f189b8ec4a8258e353e58c8f601c5868
Gasana.NHonore <ndayiho5@gmail.com>
2021-01-24 14:12:26 +0100

    mettre en place une police custom --> notion des classes custom

--- src/components/titres/TitleH1.js
+++ src/components/titres/TitleH1.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import styles from './Titre.module.css';
 
 
-const titleH1 = (props) => (
-        <h1 className="border border-dark bg-primary p-2 m-2 rounded text-center text-white">{props.children}</h1>
+const titleH1 = (props) => {
+    const mesClasses = `${styles.policeTitre} border border-dark bg-primary p-2 m-2 rounded text-center text-white`;
+    return (
+        <h1 className={mesClasses} >{props.children}</h1>
     );
+}
 
 
 export default titleH1
