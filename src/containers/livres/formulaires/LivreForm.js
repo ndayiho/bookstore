815476314da0ab450fb27614c54569603d0fdfdf
Gasana.NHonore <ndayiho5@gmail.com>
2021-02-04 22:00:15 +0100

    introduction a formik pour les formulaires

--- src/containers/livres/formulaires/LivreForm.js
+++ src/containers/livres/formulaires/LivreForm.js
@@ -1,24 +1,9 @@
 import React, { Component } from 'react';
 import classes from './LivreForm.module.css';
-import Button from '../../../components/buttons/Button'
+import Button from '../../../components/buttons/Button';
+import { withFormik } from 'formik';
 
 class LivreForm extends Component {
-    state = {
-        titreSaisi: "",
-        auteurSaisi: "",
-        nombrePagesSaisi: ""
-    }
-
-    handlerAjoutLivre =(event) =>{
-        event.preventDefault();
-        this.props.validation(this.state.titreSaisi,this.state.auteurSaisi,this.state.nombrePagesSaisi);
-        /** initialise le formulaire */
-        this.setState( {
-            titreSaisi: "",
-            auteurSaisi: "",
-            nombrePagesSaisi: ""
-        });
-    }
 
     render() {
         const stylesClasses = `${classes.formTitle} w-100 text-center text-primary`;
@@ -29,21 +14,53 @@ class LivreForm extends Component {
                 <form>
                     <div className="form-group">
                         <label htmlFor="titre">Titre du livre </label>
-                        <input type="text" className="form-control" id="titre" onChange={(event) => this.setState({ titreSaisi: event.target.value })} />
+                        <input type="text"
+                            className="form-control"
+                            id="titre"
+                            name="titre"
+                            value={this.props.values.titre}
+                            onChange={this.props.handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="auteur">Auteur du livre </label>
-                        <input type="text" className="form-control" id="auteur" onChange={(event) => this.setState({ auteurSaisi: event.target.value })} />
+                        <input type="text"
+                            className="form-control"
+                            id="auteur"
+                            name="auteur"
+                            value={this.props.values.auteur}
+                            onChange={this.props.handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="NombresPages">Nombres de pages du livre </label>
-                        <input type="text" className="form-control" id="NombresPages" onChange={(event) => this.setState({ nombrePagesSaisi: event.target.value })} />
+                        <input type="text"
+                            className="form-control"
+                            name="nbPages"
+                            value={this.props.values.nbPages}
+                            id="NombresPages" onChange={this.props.handleChange}
+                        />
                     </div>
-                    <Button btnType="btn-primary" clic={this.handlerAjoutLivre}>Valider</Button>
+                    <Button btnType="btn-primary" clic={this.props.handleSubmit}>Valider</Button>
                 </form>
             </>
         );
     }
 }
+//withFormik est appelle avant le formulare LivreForm, donc il lui transmet les props a utiliser
+export default withFormik({
+    mapPropsToValues: () => ({
+        titre: '',
+        auteur: '',
+        nbPages: ''
+    }),
 
-export default LivreForm;
+    validate: values => {
+
+    },
+
+    handleSubmit: (values, { props }) => {
+        props.validation(values.titre, values.auteur, values.nbPages);
+    }
+
+})(LivreForm);
