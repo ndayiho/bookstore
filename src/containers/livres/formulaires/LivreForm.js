cb5ef5ffc399db8d5c636efaf4d32682597082d2
Gasana.NHonore <ndayiho5@gmail.com>
2021-02-04 22:36:35 +0100

    validation dezs valeurs  avec formik

--- src/containers/livres/formulaires/LivreForm.js
+++ src/containers/livres/formulaires/LivreForm.js
@@ -20,7 +20,10 @@ class LivreForm extends Component {
                             name="titre"
                             value={this.props.values.titre}
                             onChange={this.props.handleChange}
+                            onBlur={this.props.handleBlur}
                         />
+                        {this.props.touched.titre && this.props.errors.titre &&
+                            <span style={{ color: "red" }}>{this.props.errors.titre}</span>}
                     </div>
                     <div className="form-group">
                         <label htmlFor="auteur">Auteur du livre </label>
@@ -30,18 +33,24 @@ class LivreForm extends Component {
                             name="auteur"
                             value={this.props.values.auteur}
                             onChange={this.props.handleChange}
+                            onBlur={this.props.handleBlur}
                         />
+                        {this.props.touched.auteur && this.props.errors.auteur &&
+                            <span style={{ color: "red" }}>{this.props.errors.auteur}</span>}
                     </div>
                     <div className="form-group">
                         <label htmlFor="NombresPages">Nombres de pages du livre </label>
-                        <input type="text"
+                        <input type="number"
                             className="form-control"
                             name="nbPages"
                             value={this.props.values.nbPages}
-                            id="NombresPages" onChange={this.props.handleChange}
+                            id="NombresPages"
+                            onChange={this.props.handleChange}
+                            onBlur={this.props.handleBlur}
                         />
                     </div>
-                    <Button btnType="btn-primary" clic={this.props.handleSubmit}>Valider</Button>
+                    <Button btnType="btn-primary mr-1 mb-2" clic={this.props.handleSubmit}>Valider</Button>
+                    <Button btnType="btn-primary mb-2" clic={this.props.handleReset}>Reset</Button>
                 </form>
             </>
         );
@@ -56,7 +65,19 @@ export default withFormik({
     }),
 
     validate: values => {
+        const errors = {};
+        if (values.titre.length < 3) {
+            errors.titre = "Le titre doit avoir au plus 3 caracteres";
+        }
+        if (values.titre.length > 15) {
+            errors.titre = "Le titre doit avoir au moins 15 caracteres";
+        }
 
+        if (!values.auteur) {
+            errors.auteur = "Le champ auteur est obligatoire";
+        }
+
+        return errors;
     },
 
     handleSubmit: (values, { props }) => {
